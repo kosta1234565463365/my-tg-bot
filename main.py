ae9a9b271d82eb0b0660437bd0ee1e7f72451c8c
kosta1234565463365 <kostapukosta@gmail.com>
2026-09-15 19:18:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telebot.apihelper import ApiTelegramException
 # ЧАСТЬ 1: НАСТРОЙКА БОТА И ПЕРЕМЕННЫХ
 # ==========================================
 # Бот автоматически возьмет токен из настроек Render (переменная TOKEN)
-TOKEN = "8955717735:AAEB6fi66bZXd6ff4ab31NeQqUWL5T5VSrI"
+TOKEN = "8955717735:AAE_VXv4d9z5ljXTJdk8-0Q9xZdH7vQmdFg"
 bot = telebot.TeleBot(TOKEN)
 
 # Хранилище сессий пользователей (для Единого Окна)
